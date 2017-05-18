0a0477763ec730836c4293d468a3d22f8dca7827
Tom Craig <thetomcraig@icloud.com>
2017-05-18 07:39:22 -0700

    powerlevel9k

--- zsh.sh
+++ zsh.sh
@@ -2,7 +2,10 @@
 #MY PERSONAL BASH PROFILE
 #########################
 export ZSH=${HOME}/.oh-my-zsh
-ZSH_THEME="agnoster"
+ZSH_THEME="powerlevel9k/powerlevel9k"
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir rbenv vcs)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history time)
+
 
 TERM=screen
 dotfiles_location="${HOME}/Dotfiles"
