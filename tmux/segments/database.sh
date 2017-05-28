59ebedbcf82f756e10f96120eb487ed2b4d7b48a
Tom Craig <thetomcraig@icloud.com>
2017-05-28 13:55:05 -0700

    Fixed hostname

--- tmux/segments/database.sh
+++ tmux/segments/database.sh
@@ -3,8 +3,8 @@
 db=$(python ~/slingshot-utils/slingshot_functions.py get_current_dbi $(tmux display-message -p '#S'))
 runSegment() {
     project_path=''
-    case $HOSTNAME in
-        (ZENO.local) echo "";;
+    case $(hostname) in
+        (ZENO) echo "";;
         (tcraig-m01) echo "$db  ";;
         (*) echo "How did I get in the middle of nowhere?";;
     esac
