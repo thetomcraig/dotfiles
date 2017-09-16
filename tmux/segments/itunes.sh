6a47bc7d0664b96aa13db069664ff9110ecc779c
Tom Craig <thetomcraig@icloud.com>
2017-09-16 15:30:47 -0700

    Added iTunes in again

---
+++ tmux/segments/itunes.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+ITUNES_TRACK=$(osascript <<EOF
+if appIsRunning("iTunes") then
+    tell app "iTunes" 
+ 		if player state is paused then
+            set playerStateText to "paused"
+        else if player state is playing then
+            set playerStateText to "playing"
+        else
+            set playerStateText to "ptopped"
+        end if
+		set trackName to name of the current track
+		set info to [trackName, playerStateText]
+    end tell
+end if
+on appIsRunning(appName)
+    tell app "System Events" to (name of processes) contains appName
+end appIsRunning
+EOF)
+
+runSegment() {
+	IFS=',' read -r -a TRACK_PARTS <<< "$ITUNES_TRACK"
+	INDICATOR=' '
+	if [ ${TRACK_PARTS[1]} == 'paused' ]; then
+		INDICATOR=' '
+	fi
+	if [ ${TRACK_PARTS[1]} == 'playing' ]; then
+		INDICATOR=' '
+	fi
+
+	if ((${#TRACK_PARTS[0]} > 15)); then
+		echo "${INDICATOR}${TRACK_PARTS[0]:0:13}..."
+	else
+		echo ${INDICATOR}${TRACK_PARTS[0]}
+	fi
+}
--- tmux/status.sh
+++ tmux/status.sh
@@ -8,7 +8,7 @@ displayStatus() {
     displaySegment "session" "${TMUX_LIGHT_SEGMENT_FG}" "${TMUX_LIGHT_SEGMENT_BG}"
     displaySegment "database" "${TMUX_DARK_SEGMENT_FG}" "${TMUX_DARK_SEGMENT_BG}"
   else
-    displaySegment "swinsian" "${TMUX_DARK_SEGMENT_FG}" "${TMUX_DARK_SEGMENT_BG}"
+    displaySegment "itunes" "${TMUX_DARK_SEGMENT_FG}" "${TMUX_DARK_SEGMENT_BG}"
     displaySegment "weather" "${TMUX_DARK_SEGMENT_FG}" "${TMUX_DARK_SEGMENT_BG}"
     displaySegment "time" "${TMUX_DARK_SEGMENT_FG}" "${TMUX_DARK_SEGMENT_BG}"
     displaySegment "battery" "${TMUX_DARK_SEGMENT_FG}" "${TMUX_DARK_SEGMENT_BG}"
