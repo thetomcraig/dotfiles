77d7a7e45bc976691907396dbf49588dc9e3561b
Tom Craig <thetomcraig@icloud.com>
2017-10-28 09:21:24 -0700

    Fixed the check script

--- check.sh
+++ check.sh
@@ -1,9 +1,7 @@
 git fetch origin master
 changes=$(git log master..origin/master --oneline | awk '{print $1}')
-echo $changes
-num_changes=${#changes[@]}
-echo $num_changes
-if [ $num_changes -gt 1 ]
+num_changes=${#changes}
+if [ $num_changes -gt 0 ]
 then
     echo "Dot files have been updated on remote! Would you like to pull? [y|n]:" 
     read answer
