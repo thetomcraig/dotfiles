2cca3abe702c97f5c802475d59b6a19c3aa13908
Tom Craig <tcraig@pandora.com>
2018-02-02 09:20:30 -0800

    New stuff

--- vundle_settings.sh
+++ vundle_settings.sh
@@ -9,26 +9,26 @@ call vundle#begin()
 " let Vundle manage Vundle, required "
 Plugin 'VundleVim/Vundle.vim'
 
-Plugin 'ap/vim-buftabline'
+"Plugin 'ap/vim-buftabline'"
 Plugin 'mileszs/ack.vim'
 Plugin 'vim-airline/vim-airline'
 Plugin 'easymotion/vim-easymotion'
 Plugin 'tpope/vim-fugitive'
 Plugin 'tpope/vim-dispatch'
-Plugin 'airblade/vim-gitgutter'
-Plugin 'scrooloose/nerdtree'
-Plugin 'Shougo/neocomplete.vim'
-Plugin 'kshenoy/vim-signature'
 Plugin 'tpope/vim-surround'
 Plugin 'tpope/vim-unimpaired'
+Plugin 'airblade/vim-gitgutter'
+Plugin 'scrooloose/nerdtree'
+Plugin 'scrooloose/nerdcommenter'
+Plugin 'Shougo/neocomplete.vim'
+Plugin 'kshenoy/vim-signature'
 Plugin 'majutsushi/tagbar'
 Plugin 'w0rp/ale'
 Plugin 'maxbrunsfeld/vim-yankstack'
 Plugin 'junegunn/fzf'
-"Plugin 'gko/vim-coloresque'"
-Plugin 'inkarkat/vim-mark'
-Plugin 'scrooloose/nerdcommenter'
+Plugin 'thetomcraig/Mark--Karkat'
 Plugin 'ryanoasis/vim-devicons'
+"Plugin 'jeetsukumaran/vim-buffergator'
 
 
 " All of your Plugins must be added before the following line "
