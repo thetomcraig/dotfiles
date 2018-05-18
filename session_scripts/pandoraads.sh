c3ae5616ead1761b2c32734145b9f26ca5a60314
Tom Craig <tcraig@pandora.com>
2018-05-18 10:51:16 -0700

    Add pandoraads script

---
+++ session_scripts/pandoraads.sh
@@ -0,0 +1,28 @@
+export EVENT_NOKQUEUE=1
+
+cd $1
+tmux -u new-session -d -s $2
+
+tmux new-window -t $2:1 -n Graphql
+tmux new-window -t $2:2 -n NPM
+tmux new-window -t $2:3 -n Text
+tmux new-window -t $2:4 -n ZSH 
+
+tmux select-window -t produktizr:1
+
+tmux send-keys -t $2:1 "cd $1" C-m C-m
+tmux send-keys -t $2:1 "cd $2" C-m C-m
+tmux send-keys -t $2:1 "nvm use 8.9.4" C-m C-m
+tmux send-keys -t $2:1 "npm run graphql" C-m C-m
+
+tmux send-keys -t $2:2 "cd $1" C-m C-m
+tmux send-keys -t $2:2 "cd $2" C-m C-m
+tmux send-keys -t $2:2 "nvm use 8.9.4" C-m C-m
+tmux send-keys -t $2:2 "npm start" C-m C-m
+
+tmux send-keys -t $2:3 "cd $1" C-m C-m
+tmux send-keys -t $2:3 "cd $2/src" C-m C-m
+tmux send-keys -t $2:3 "vim" C-m C-m
+
+tmux send-keys -t $2:4 "cd $1" C-m C-m
+tmux send-keys -t $2:4 "cd $2" C-m C-m
