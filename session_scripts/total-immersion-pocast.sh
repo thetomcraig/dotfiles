35840faa92506f3c2a56829a8100175e18461524
Tom Craig <thetomcraig@icloud.com>
2018-06-16 16:24:21 -0700

    Remov old total immersion file

--- session_scripts/total-immersion-pocast.sh
+++
@@ -1,7 +0,0 @@
-export EVENT_NOKQUEUE=1
-
-tmux -u new-session -d -s 'total-immersion-podcast'
-
-tmux new-window -t $1:1 -n Text
-tmux send-keys -t $name:1 "cd ~/Dropbox/TomCraig/Projects/total-immersion-podcast/" C-m C-m
-tmux send-keys -t $name:1 "spacemacs" C-m C-m
