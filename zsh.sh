38702a94b8ca9e490108ef73b85223428dfa2a40
Tom Craig <thetomcraig@icloud.com>
2018-06-17 23:02:38 -0700

    Section things properly and improve speed and reliability; add echos

--- zsh.sh
+++ zsh.sh
@@ -5,13 +5,26 @@ export ZSH=${HOME}/.oh-my-zsh
 scripts_location="${HOME}/Dropbox/TomCraig/Scripts"
 dotfiles_location="${HOME}/Dotfiles"
 tmux_session_scripts_dir="$dotfiles_location/session_scripts"
-export tmux_dotfiles_location="$dotfiles_location/tmux"
+tmux_dotfiles_location="$dotfiles_location/tmux"
 source $tmux_dotfiles_location/tmux_colors.sh
 
+
+
+# GENERAL STUFF
+echo "BEGIN INITIALIZATION..."
 export LANG="en_US.UTF-8"
 export LC_ALL=en_US.UTF-8
 export TERM=xterm-256color
+export FZF_DEFAULT_COMMAND='ag -g ""'
+# Keep 1000 lines of history within the shell and save it to ~/.zsh_history:
+HISTSIZE=1000
+SAVEHIST=1000
+HISTFILE=~/.zsh_history
 
+
+
+# ZSH STUFF
+echo "LOADING ZSH..."
 ZSH_THEME="powerlevel9k/powerlevel9k"
 POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(virtualenv context dir)
 POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
@@ -20,12 +33,6 @@ POWERLEVEL9K_SHORTEN_STRATEGY="truncate_from_right"
 POWERLEVEL9K_DISABLE_RPROMPT=true
 POWERLEVEL9K_VIRTUALENV_BACKGROUND=green
 
-# Keep 1000 lines of history within the shell and save it to ~/.zsh_history:
-#HISTCONTROL=ignoredups
-HISTSIZE=1000
-SAVEHIST=1000
-HISTFILE=~/.zsh_history
-
 plugins=(git, zsh-autosuggestions)
 setopt auto_cd
 bindkey '^j' autosuggest-accept
@@ -33,17 +40,33 @@ DISABLE_AUTO_TITLE=true
 
 source $ZSH/oh-my-zsh.sh
 
-alias s="source $dotfiles_location/zsh.sh"
+
+
+# CD ALIASES
+echo "LOADING ALIASES AND FUNCTIONS..."
+alias v="vim $dotfiles_location/vimrc.sh"
+alias vv="vim +"NERDTree $1""
+alias vu="vim $dotfiles_location/vundle_settings.sh"
+alias sc="cd ${HOME}/Dropbox/TomCraig/Scripts"
+alias dot="cd ${HOME}/Dotfiles"
+
+
+
+# GENERAL AIASES
 alias ctags="`brew --prefix`/bin/ctags"
+alias h="history"
+alias hg="history | grep $1"
+alias cpb="pwd | pbcopy"
+alias launch="$dotfiles_location/launch_projects.sh"
 
 #t for tree
 alias t=mytree
 mytree() {
-	#this does tree with 1 level
-	#if no first parameter,
-	#and otherwise uses the
-	#supplied paramter for level depth
-	tree -C -L ${1-1} -I "*.pyc"
+  #this does tree with 1 level
+  #if no first parameter,
+  #and otherwise uses the
+  #supplied paramter for level depth
+  tree -C -L ${1-1} -I "*.pyc"
 }
 #T for tree 1 level deep with no invisibles
 alias T="tree -a -C -L 1"
@@ -53,6 +76,10 @@ unalias d
 alias d="tree -a -C -L 1 -d"
 alias s='source $dotfiles_location/zsh.sh'
 alias b='vim $dotfiles_location/zsh.sh'
+
+
+
+# GIT ALIASES AND FUNCTIONS
 alias g="git status"
 alias ga="git add"
 alias gb="git branch"
@@ -67,8 +94,14 @@ alias gdd="git ls-files --deleted -z | xargs -0 git rm"
 alias gm="git commit -m "
 alias gpu="git push"
 alias gpl="git pull"
-alias detatched_commits="git log --oneline --all --graph --decorate  $(git reflog | awk '{print $1}')"
 alias log="git log --oneline --all --graph --decorate"
+
+# Show detatched commits
+alias gdc=detatched_commits
+detatched_commits() {
+  git log --oneline --all --graph --decorate  $(git reflog | awk '{print $1}')
+}
+# Make local branch tracking remote that matches regex
 alias gg=gitbranchgrep
 gitbranchgrep() {
   git pull && \
@@ -76,71 +109,73 @@ gitbranchgrep() {
   git branch -vv
 }
 
+# Open conflicted files in vim
+alias vc=vim_conflicted_files
+vim_conflicted_files() {
+  git_root=$(git rev-parse --show-toplevel)
+  conflicted_files=$(git diff --name-only --diff-filter=U)
+  full_path_file_list=()
+  for f in $conflicted_files; do
+      full_path_file_list+="$git_root/$f "
+  done
+  vim $full_path_file_list
+}
 
-alias vc="vim $($scripts_location/get_conflicted_files.sh)"
-alias h="history"
-alias hg="history | grep $1"
-alias c="clear "
-alias cpb="pwd | pbcopy"
 
+
+# TMUX ALIASES AND FUNCTIONS
 alias tmux="tmux -u"
 alias ta="tmux a -t"
 alias ts="tmux ls"
 alias td="tmux detatch"
 alias tk="tmux kill-session -t"
-alias tk="tmux kill-session -t $(tmux display-message -p '#S')"
+alias tm="vim $dotfiles_location/tmux/tmux.conf"
 alias trn="tmux rename-window $1"
 alias trv="tmux select-layout even-vertical"
 alias trh="tmux select-layout even-horizontal"
 
+alias tkk=kill_current_session
+kill_current_session() {
+  tmux kill-session -t $(tmux display-message -p '#S')
+}
+
+
+
+# PYTHON ALISES
 alias vev="virtualenv env"
 alias seba="source env/bin/activate"
 alias pf="pip freeze"
 alias p="python $scripts_location/start_ipython.py"
 
-alias b="vim $dotfiles_location/zsh.sh"
-alias v="vim $dotfiles_location/vimrc.sh"
-alias vv="vim +"NERDTree $1""
-alias vu="vim $dotfiles_location/vundle_settings.sh"
-alias tm="vim $dotfiles_location/tmux/tmux.conf"
-alias sc="cd ${HOME}/Dropbox/TomCraig/Scripts"
-alias dot="cd ${HOME}/Dotfiles"
 
-# Media
-alias mo="cd ${HOME}/Dropbox/TomCraig/Movies"
-alias mu="cd ${HOME}/Dropbox/TomCraig/Music"
-alias tv="cd ${HOME}/Dropbox/TomCraig/TV"
 
-alias launch="$dotfiles_location/launch_projects.sh"
-
-# SSH
+# SSH ALIASES
 alias sshariston="ssh tom@10.0.1.3"
 
-export FZF_DEFAULT_COMMAND='ag -g ""'
 
+
+# ENVIRONMENT SETTINGS
+echo "LOADING ENVIRONMENT SETTINGS..."
 host=$(uname -a)
+# MACOS
 if [[ $host == *"Darwin"* ]]; then
     alias rm="trash"
     alias cat="ccat"
 fi
-# Do this if in a tmux session
-if ! { [ "$TERM" = "screen" ] && [ -n "$TMUX" ]; } then
+# TMUX SESSION
+if [[ "$TERM" =~ "screen".* ]]; then
   $tmux_session_scripts_dir/setup_env_vars.sh $(tmux display-message -p '#S')
 fi
 
-# Quick commands to sync CWD between terminals.
-pin() {
-  rm -f ~/.pindir
-  echo $PWD >~/.pindir
-  chmod 0600 ~/.pindir >/dev/null 2>&1
-}
-pout() {
-  cd `cat ~/.pindir`
-}
 
-eval $(thefuck --alias)
 
+# LOAD EXTERNAL STUFF
+echo "LOADING PYENV..."
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
+
+
+
+echo "INITIALIZATION COMPLETE"
