960a2013ba8add61a5fa26c3a6d8a7adca1b5d2f
Tom Craig <tcraig@pandora.com>
2018-06-26 15:11:04 -0700

    Change to "npm start"

--- session_scripts/pandora-harmony.sh
+++ session_scripts/pandora-harmony.sh
@@ -10,7 +10,7 @@ tmux select-window -t $name:1
 tmux send-keys -t $name:1 "cd $path" C-m C-m
 tmux send-keys -t $name:1 "load_nvm" C-m C-m
 tmux send-keys -t $name:1 "nvm use 8.9.4" C-m C-m
-tmux send-keys -t $name:1 "npm run styleguide" 
+tmux send-keys -t $name:1 "npm start" 
 
 tmux send-keys -t $name:2 "cd $path" C-m C-m
 tmux send-keys -t $name:2 "vim" C-m C-m
