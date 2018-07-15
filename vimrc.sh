8a9c017ef708e08d39449d58079200bda04dfbcc
Tom Craig <thetomcraig@icloud.com>
2018-07-14 18:59:26 -0700

    Add livedown and easy align settings

--- vimrc.sh
+++ vimrc.sh
@@ -57,6 +57,10 @@ nmap <Space>s{ ysiw{
 nmap <Space>s' ysiw'
 nmap <Space>s" ysiw"
 
+" Start interactive EasyAlign in visual mode (e.g. vipga)
+xmap ga <Plug>(EasyAlign)
+" Start interactive EasyAlign for a motion/text object (e.g. gaip)
+nmap ga <Plug>(EasyAlign)
 
 nmap <Space>w/ :vsplit<CR>
 nmap <Space>w\ :vsplit<CR>
@@ -262,6 +266,7 @@ call airline#add_inactive_statusline_func('WindowNumber')
 " let g:airline_section_y = airline"section"create(['%f'])
 "let g:airline_section_z = ''
 
+let g:livedown_browser = "safari"
 let g:goyo_height=100
 let g:goyo_width=125
 nnoremap <silent> <leader>z :Goyo<cr>
