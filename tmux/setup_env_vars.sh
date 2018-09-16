b0206de234671a570dd9a537cf1268597c157116
Tom Craig <thetomcraig@icloud.com>
2018-09-15 21:41:22 -0700

    Add root alias

--- tmux/setup_env_vars.sh
+++ tmux/setup_env_vars.sh
@@ -1,31 +1,17 @@
 dotfiles_location=$(cd "$(dirname "$0")" && cd .. && pwd)
 source $dotfiles_location/helper_functions.sh
 
-case $1 in
+project_name=${1}
+echo "project_name"
+echo ${project_name}
+
+project_root=~/.projects_root/${project_name}
+alias root="cd ${project_root}"
+
+
+
+case ${project_name} in
 total-immersion-podcast)
-  alias post=open_tabs_for_posting
-  open_tabs_for_posting() {
-    xml_url="https://raw.githubusercontent.com/thetomcraig/total-immersion-podcast/master/xml_stuff/itunes.xml"
-    podtrac_url="http://publisher.podtrac.com/dashboard/byT0zgJ3o_Jv"
-    s3_url="https://console.aws.amazon.com/s3/buckets/total-immersion-podcast/?region=us-west-2&tab=overview#"
-    validator_url="http://castfeedvalidator.com/"
-    stitcher_url="https://partners.stitcher.com/dashboard.php"
-    itunes_url="https://podcastsconnect.apple.com/my-podcasts"
-    soundcloud_url="https://soundcloud.com/upload"
-    declare -a web_pages=(
-      $xml_url
-      $podtrac_url
-      $s3_url
-      $validator_url
-      $stitcher_url
-      $itunes_url
-      $soundcloud_url
-    )
-    for i in "${web_pages[@]}"
-    do
-      open $i
-    done
-  }
   echoGreen "TOTAL IMMERSION ENGAGED"
   ;;
 *)
