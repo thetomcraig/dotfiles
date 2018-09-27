4a8a7bcd9f2f43cae45fe100056bb94eb4528d81
Tom Craig <tcraig@pandora.com>
2018-09-26 19:46:10 -0700

    Add destroy script - initial check in

---
+++ install_scripts/destroy.sh
@@ -0,0 +1,7 @@
+# Repo all my shit before dumping a computer
+# In the future, clone personal stuff to a different dir than company stuff
+
+ls ~/.projects_root
+echo "Make sure dev projects have no outstanding commits"
+echo "Run this: 'rm -rf ${HOME}/.projects_root'"
+
