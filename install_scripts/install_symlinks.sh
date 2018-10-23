1bbce7f1401caf44ebf0baec5d0dc1730a6e00fb
thetomcraig <thetomcraig@users.noreply.github.com>
2018-10-22 21:45:22 -0700

    Small changes after Welkin

--- install_scripts/install_symlinks.sh
+++ install_scripts/install_symlinks.sh
@@ -7,14 +7,15 @@ elif [[ $host == *"ZENO"* ]]; then
   projects_root="${HOME}/Dropbox/TomCraig/Projects"
 elif [[ $host == *"HERMES"* ]]; then
   projects_root="${HOME}"
+else
+   projects_root="${HOME}"
 fi
 
-#Link Files
-#Make sure these point to actual paths
+# Link Files
 cd ..
 dotfiles_location=$(pwd)
-cd ~
 
+cd ~
 rm -f -- ~/.projects_root
 ln -s $projects_root .projects_root
 rm -f -- ~/.editorconfig
@@ -27,5 +28,7 @@ rm -f -- ~/.vim/spell
 ln -s $dotfiles_location/.vim/spell .vim/spell
 rm -f -- ~/.tmux.conf
 ln -s $dotfiles_locationtmux/tmux.conf .tmux.conf
-rm -f -- ~/.hammerspoon/init.lua
-cat "../init.lua" >> ~/.hammerspoon/init.lua
+
+cd ~/Library/Preferences/
+rm -f -- ~/Library/Preferences/com.manytricks.Moom.plist
+ln -s $dotfiles_locationtmux/gui_apps/com.manytricks.Moom.plist com.manytricks.Moom.plist
