505dc58c7c40444144a3d1d1a64e3ba12c22561d
Tom Craig <thetomcraig@icloud.com>
2018-10-22 21:45:59 -0700

    Add packages txt and remove old sh files

--- install_scripts/install_manual_steps.sh
+++
@@ -1,8 +0,0 @@
-echo "Disable Capslock"
-echo "Disable CMD+Space for spotlight"
-echo "Set karabiner elements, capslock - f18"
-echo "Setup Dropbox Selective Sync"
-echo "Activate Alfred and point settings file at dropbox"
-echo "Get app specific password for fantastical"
-
-echo "Add these to login items: [Moom, Flux, Fantastical, Dropbox, Karabiner, HammerSpoon]"
