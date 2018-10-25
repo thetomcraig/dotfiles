3be044753e4b7bff2a74ea09e51047084e8a3961
Tom Craig <thetomcraig@icloud.com>
2018-10-25 11:08:47 -0700

    Add welkin settings and tmux setup

---
+++ welkinrc.sh
@@ -0,0 +1,6 @@
+  export WELKIN_ROOT="${HOME}/dev/welkin"
+  export WELKIN_VIRTUALENV_PATH="${HOME}/dev/welkin_2_7_env/"
+  export CONFIG='dev.ini'
+  alias envactivate="source $WELKIN_VIRTUALENV_PATH/bin/activate"
+  alias enter_welkin_env="cd $WELKIN_ROOT; envactivate"
+
