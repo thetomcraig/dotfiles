63cc1f0841e376f1cdd5edaa2998c2008cd97f95
Tom Craig <thetomcraig@icloud.com>
2018-11-18 18:24:28 -0800

    Updated since welkin

--- editorconfig.sh
+++ editorconfig.sh
@@ -1,30 +1,31 @@
-# http://editorconfig.org
+# EditorConfig is awesome: https://EditorConfig.org
 
 # top-most EditorConfig file
 root = true
 
-[*.md]
-trim_trailing_whitespace = false
-indent_size = 2
+# Unix-style newlines with a newline ending every file
+[*]
+end_of_line = lf
+insert_final_newline = true
 
+# Matches multiple files with brace expansion notation
+# Set default charset
+[*.{js,py}]
+charset = utf-8
+
+# 4 space indentation
 [*.py]
-trim_trailing_whitespace = false
+indent_style = space
 indent_size = 4
 
-[*.js]
-trim_trailing_whitespace = true
-indent_size = 2
-
-[*.json]
-trim_trailing_whitespace = true
-indent_size = 2
-
-[*.sh]
-trim_trailing_whitespace = true
-indent_size = 2
-
-[*]
+[*{.md,.js}]
 indent_style = space
-end_of_line = lf
-charset = utf-8
-max_line_length = 100
+indent_size = 2
+
+[{package.json,.travis.yml}]
+indent_style = space
+indent_size = 2
+
+# Tab indentation (no size specified)
+[Makefile]
+indent_style = tab
