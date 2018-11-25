ad7cdbced69cb326ad4af1f55cc57175ee02b7e0
Tom Craig <thetomcraig@icloud.com>
2018-11-25 14:48:51 -0800

    Add more shit

--- install_scripts/install_symlinks.sh
+++ install_scripts/install_symlinks.sh
@@ -18,16 +18,23 @@ dotfiles_location="${HOME}"/dotfiles
 cd ~
 rm -f -- ~/.projects_root
 ln -s $projects_root .projects_root
+
 rm -f -- ~/.editorconfig
 ln -s $dotfiles_location/editorconfig.sh .editorconfig
+
 rm -f -- ~/.zshrc
 ln -s $dotfiles_location/zsh.sh .zshrc
+
 rm -f -- ~/.vimrc
 ln -s $dotfiles_location/vim.sh .vimrc
+
 rm -f -- ~/.vim/spell
 ln -s $dotfiles_location/.vim/spell .vim/spell
+
 rm -f -- ~/.tmux.conf
-ln -s $dotfiles_locationtmux/tmux.conf .tmux.conf
+ln -s $dotfiles_location/tmux/tmux.conf .tmux.conf
+
+ln -s $dotfiles_location/diff-so-fancy /usr/local/bin/diff-so-fancy
 
 cd ~/Library/Preferences/
 rm -f -- ~/Library/Preferences/com.manytricks.Moom.plist
