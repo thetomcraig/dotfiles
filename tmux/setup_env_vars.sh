ee4dd3c4fcddfef8a0daa7ad02acd10a7cca0436
Tom Craig <thetomcraig@icloud.com>
2019-01-17 11:40:04 -0800

    Reorganize

--- tmux/setup_env_vars.sh
+++ tmux/setup_env_vars.sh
@@ -1,15 +1,20 @@
-dotfiles_location=$(cd "$(dirname "$0")" && cd .. && pwd)
-source $dotfiles_location/helper_functions.sh
+# This is used with my Tmux workflow
+# When the login shell start,
+# it checks if it is in the context of a tmux session
+# If so, it runs this with the tmux session name
+# Something like this:
+#   source setup_env_vars.sh $(tmux display-message -p '#S')
 
 project_name=${1}
 echo "project_name"
-echo ${project_name}
 
-project_root=~/.projects_root/${project_name}
-alias root="cd ${project_root}"
+# Set env vars
+export ROOT=~/.projects_root/${project_name}
+alias rr="cd ${ROOT}"
+alias root="cd ${ROOT}"
 
 
-
+# Do specific stuff here
 case ${project_name} in
 total-immersion-podcast)
   echoGreen "TOTAL IMMERSION ENGAGED"
