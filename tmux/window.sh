ac184b463a3634bf81cf11e00b7eca7475f806a5
Tom Craig <thetomcraig@icloud.com>
2019-02-21 14:40:42 -0800

    Add logic for zoomed windows

--- tmux/window.sh
+++ tmux/window.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 if [[ $1 == "current" ]]; then
-  echo "#[fg=${TMUX_CURRENT_WINDOW_FG},bg=${TMUX_CURRENT_WINDOW_BG}] #I | #W "
+  FOREGROUND=${TMUX_CURRENT_WINDOW_FG}
+  BACKGROUND=${TMUX_CURRENT_WINDOW_BG}
+
+  # Check if this window is zoomed
+  ACTIVE_PANE_IS_ZOOMED=$(tmux list-panes -F '#F' | grep 'Z')
+  if [ -n "${ACTIVE_PANE_IS_ZOOMED}" ]; then
+    FOREGROUND=${TMUX_CURRENT_WINDOW_FG_ZOOMED}
+    BACKGROUND=${TMUX_CURRENT_WINDOW_BG_ZOOMED}
+  fi
+  
+  echo "#[fg=${FOREGROUND},bg=${BACKGROUND}] #I | #W "
+
 else
   echo "#[fg=${TMUX_WINDOW_FG},bg=${TMUX_WINDOW_BG}] #I | #W "
 fi
