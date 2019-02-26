d7c5339cf26b8d71084f9c8fccb92622e6208bdb
Tom Craig <thetomcraig@icloud.com>
2019-02-26 09:59:53 -0800

    Fix zoom color

--- tmux/window.sh
+++ tmux/window.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
+# Default 
+FOREGROUND_COLOR="${TMUX_WINDOW_FG}"
+FOREGROUND_COLOR="${TMUX_WINDOW_FG}"
+
+# Overide - color the current window
 if [[ $1 == "current" ]]; then
-  FOREGROUND=${TMUX_CURRENT_WINDOW_FG}
-  BACKGROUND=${TMUX_CURRENT_WINDOW_BG}
-
-  # Check if this window is zoomed
-  ACTIVE_PANE_IS_ZOOMED=$(tmux list-panes -F '#F' | grep 'Z')
+  FOREGROUND_COLOR=${TMUX_CURRENT_WINDOW_FG}
+  BACKGROUND_COLOR=${TMUX_CURRENT_WINDOW_BG}
+  # Overide - color the current window with the ZOOM color
+  ACTIVE_PANE_IS_ZOOMED=$(tmux list-panes -F '#F' | grep '*Z')
   if [ -n "${ACTIVE_PANE_IS_ZOOMED}" ]; then
-    FOREGROUND=${TMUX_CURRENT_WINDOW_FG_ZOOMED}
-    BACKGROUND=${TMUX_CURRENT_WINDOW_BG_ZOOMED}
+    FOREGROUND_COLOR=${TMUX_CURRENT_WINDOW_FG_ZOOMED}
+    BACKGROUND_COLOR=${TMUX_CURRENT_WINDOW_BG_ZOOMED}
   fi
-  
-  echo "#[fg=${FOREGROUND},bg=${BACKGROUND}] #I | #W "
-
-else
-  echo "#[fg=${TMUX_WINDOW_FG},bg=${TMUX_WINDOW_BG}] #I | #W "
 fi
+
+echo "#[fg=${FOREGROUND_COLOR},bg=${BACKGROUND_COLOR}] #I | #W "
