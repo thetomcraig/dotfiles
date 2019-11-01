519a23531d2a8a53794cec2ef507c2ffcb60af02
Tom Craig <thetomcraig@icloud.com>
2019-11-01 13:24:59 -0700

    pyenv settings

--- zsh_python_settings.sh
+++ zsh_python_settings.sh
@@ -4,3 +4,10 @@ alias seba="source env/bin/activate"
 alias pf="pip freeze"
 alias pyclean="git clean -fd && find . -name '*.pyc' | xargs rm && find . -name '__pycache__' | xargs rm -rf"
 alias make_python_tags="ctags -R --fields=+l --languages=python --python-kinds=-iv -f ./tags $(python -c "import os, sys; print(' '.join('{}'.format(d) for d in sys.path if os.path.isdir(d)))")"
+
+# export PYENV_ROOT="$HOME/.pyenv"
+# export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv virtualenv-init -)"
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
