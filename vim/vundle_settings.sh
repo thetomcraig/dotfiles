978b47b151f1708d8c2c97ff9a90e0ab42b9aac5
Tom Craig <thetomcraig@icloud.com>
2020-01-13 21:23:03 -0800

    Nerdtree stuff, mark, goy

--- vim/vundle_settings.sh
+++ vim/vundle_settings.sh
@@ -13,16 +13,16 @@ call vundle#begin()
 Plugin 'VundleVim/Vundle.vim'
 
 " Writing Plaintext and Markdown "
-"Plugin 'shime/vim-livedown'
+Plugin 'shime/vim-livedown'
 Plugin 'vimwiki/vimwiki'
+Plugin 'junegunn/goyo.vim'
 
 " Searching "
 Plugin 'mhinz/vim-grepper'
 Plugin 'junegunn/fzf'
 Plugin 'junegunn/fzf.vim'
 Plugin 'scrooloose/nerdtree'
-Plugin 'tiagofumo/vim-nerdtree-syntax-highlight'
-"Plugin 'vim-scripts/nerdtree-ack'
+Plugin 'vim-scripts/nerdtree-ack'
 
 " Colors/UI "
 Plugin 'junegunn/seoul256.vim'
@@ -32,38 +32,33 @@ Plugin 'vim-airline/vim-airline'
 Plugin 'vim-airline/vim-airline-themes'
 Plugin 'ryanoasis/vim-devicons'
 Plugin 'simeji/winresizer'
-"Plugin 'gcmt/taboo.vim'
+Plugin 'thetomcraig/Mark--Karkat'
 
 " Extra Functionality "
 Plugin 'easymotion/vim-easymotion'
 "Plugin 'Shougo/neocomplete.vim'
-"Plugin 'SirVer/ultisnips'
 Plugin 'tpope/vim-dispatch'
 Plugin 'tpope/vim-abolish'
 
 " Coding "
 Plugin 'w0rp/ale'
 Plugin 'scrooloose/nerdcommenter'
-"Plugin 'tpope/vim-surround'
+Plugin 'tpope/vim-surround'
 Plugin 'tpope/vim-unimpaired'
-"Plugin 'wakatime/vim-wakatime'
 
 " Git "
 Plugin 'airblade/vim-gitgutter'
 Plugin 'tpope/vim-fugitive'
-"Plugin 'idanarye/vim-merginal'
 
 " Syntax "
 Plugin 'pangloss/vim-javascript'
 Plugin 'mxw/vim-jsx'
 Plugin 'styled-components/vim-styled-components'
 Plugin 'othree/html5.vim'
-"Plugin 'ap/vim-css-color'
 Plugin 'tmux-plugins/vim-tmux'
 Plugin 'leafgarland/typescript-vim'
 Plugin 'lepture/vim-jinja'
 "Plugin 'Valloric/MatchTagAlways'
-
 
 
 " All of your Plugins must be added before the following line "
