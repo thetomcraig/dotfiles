a7cf8caea606aad6b2d91bba3e422e5804f581d5
Tom Craig <thetomcraig@icloud.com>
2020-01-13 21:17:27 -0800

    Simplify project settings

--- zsh_environment_settings.sh
+++ zsh_environment_settings.sh
@@ -1,9 +1,9 @@
 # vim: set syntax=zsh
 # au BufReadPost zsh.sh set ft=zsh.rc
 
-#############################
-# ENVIRONMENT SETTINGS (HOST)
-#############################
+#########
+# HOST/OS
+#########
 host=$(uname -a)
 if [[ $host == *"Darwin"* ]]; then
   alias rm="trash"
@@ -11,23 +11,23 @@ if [[ $host == *"Darwin"* ]]; then
   alias diff="diff-so-fancy"
 
   export dropbox_root="~/Dropbox/TomCraig"
-  export projects_root="$dropbox_root/Projects"
-fi
-if [[ $host == *"lebowski"* ]]; then
-  source $dotfiles_location/welkin/welkinrc.sh
-  export projects_root="~/dev/"
-  # export dropdbs="psql -l | grep 'unittest' | awk '{split($0, a,"|"); print a[1]}' | while read name; do dropdb $name; done"
-  echo "  WELKIN ENVIRONMENT LOADED"
 fi
 
-
-
-#####################################
-# ENVIRONMENT SETTINGS (TMUX SESSION)
-#####################################
-if [ -n "$TMUX" ]; then
-  tmux_session_name=$(tmux display-message -p '#S')
-  source $dotfiles_location/tmux/project_settings.sh ${tmux_session_name}
-fi
+export projects_root="$dropbox_root/Projects"
+export ROOT="${HOME}/.projects_root/${project_name}"
+alias rr="cd ${ROOT}"
+alias root="cd ${ROOT}"
 
 export wiki_root="$projects_root/tomcraigslist"
+
+
+
+##################
+# TMUX ENVIRONMENT
+##################
+# If in a tmux session, attempt to load the project's settings
+if [ -n "$TMUX" ]; then
+  tmux_session_name=$(tmux display-message -p '#S')
+  source "${ROOT}/.tmux_settings.sh"
+fi
+
