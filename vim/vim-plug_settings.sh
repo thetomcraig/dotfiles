0aa2b2ef4b3a7ed4605a9a4428d0fbccd4679e5e
Tom Craig <thetomcraig@icloud.com>
2020-05-03 13:57:57 -0700

    Organize plugins, add new colorscheme

--- vim/vim-plug_settings.sh
+++ vim/vim-plug_settings.sh
@@ -4,27 +4,32 @@ au BufReadPost vim-plug_settings.sh set ft=vim.rc
 call plug#begin('~/.vim/plugged')
 
 " Extra Functionality "
-Plug 'easymotion/vim-easymotion'
 Plug 'tpope/vim-dispatch'
+Plug 'tpope/vim-obsession'
+
+
+" Coding and Text "
+Plug 'w0rp/ale'
 Plug 'tpope/vim-abolish'
-Plug 'MattesGroeger/vim-bookmarks'
+Plug 'easymotion/vim-easymotion'
 Plug 'tpope/vim-repeat'
 Plug 'wellle/targets.vim'
-Plug 'w0rp/ale'
 Plug 'preservim/nerdcommenter'
 Plug 'tpope/vim-surround'
 Plug 'tpope/vim-unimpaired'
+
+
+" Visual Tools "
 Plug 'preservim/nerdtree'
 Plug 'zackhsi/nerdtree-rg.vim'
 Plug 'mbbill/undotree'
-Plug 'tpope/vim-obsession'
+Plug 'MattesGroeger/vim-bookmarks'
 Plug 'tecfu/YankRing.vim'
-Plug 'ryanoasis/vim-devicons'
-Plug 'simeji/winresizer'
-Plug 'thetomcraig/Mark--Karkat'
 Plug 'majutsushi/tagbar'
+Plug 'ryanoasis/vim-devicons'
+Plug 'thetomcraig/Mark--Karkat'
 Plug 'lfv89/vim-interestingwords'
-Plug 'jeetsukumaran/vim-pythonsense'
+Plug 'simeji/winresizer'
 
 
 " Searching "
@@ -33,10 +38,9 @@ Plug 'junegunn/fzf'
 Plug 'junegunn/fzf.vim'
 
 
-" Colors/UI "
+" Colors "
 Plug 'whatyouhide/vim-gotham'
 Plug 'morhetz/gruvbox'
-Plug 'davidosomething/vim-colors-meh'
 Plug 'jaredgorski/fogbell.vim'
 Plug 'liuchengxu/space-vim-theme'
 Plug 'junegunn/seoul256.vim'
@@ -45,11 +49,10 @@ Plug 'thetomcraig/xenomorph.vim'
 Plug 'ayu-theme/ayu-vim'
 Plug 'arcticicestudio/nord-vim'
 Plug 'cocopon/iceberg.vim'
+Plug 'AlessandroYorba/Alduin'
 
-Plug 'sheerun/vim-polyglot'
-Plug 'vim-python/python-syntax'
 
-" Writing Plaintext and Markdown "
+" Markdown "
 Plug 'shime/vim-livedown'
 Plug 'vimwiki/vimwiki'
 Plug 'sedm0784/vim-you-autocorrect'
@@ -58,6 +61,7 @@ Plug 'aklt/plantuml-syntax'
 Plug 'scrooloose/vim-slumlord'
 Plug 'jszakmeister/markdown2ctags'
 
+
 " Git "
 Plug 'airblade/vim-gitgutter'
 Plug 'tpope/vim-fugitive'
@@ -65,14 +69,18 @@ Plug 'idanarye/vim-merginal'
 Plug 'gregsexton/gitv'
 Plug 'rbong/vim-flog'
 
+
 " Syntax "
 Plug 'pangloss/vim-javascript'
 Plug 'mxw/vim-jsx'
 Plug 'othree/html5.vim'
 Plug 'tmux-plugins/vim-tmux'
 Plug 'leafgarland/typescript-vim'
-Plug 'lepture/vim-jinja'
 Plug 'ekalinin/dockerfile.vim'
+Plug 'jeetsukumaran/vim-pythonsense'
+Plug 'sheerun/vim-polyglot'
+Plug 'vim-python/python-syntax'
+Plug 'lepture/vim-jinja'
 
 
 call plug#end()
