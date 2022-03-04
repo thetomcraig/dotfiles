81fed471ace7b7f66f1f57812edb708eb5724fdb
Tom Craig <thetomcraig@icloud.com>
2022-03-04 14:30:08 -0800

    conda

--- zsh_python_settings.sh
+++ zsh_python_settings.sh
@@ -5,7 +5,16 @@ alias pf="pip freeze"
 alias pyclean="git clean -fd && find . -name '*.pyc' | xargs rm && find . -name '__pycache__' | xargs rm -rf"
 alias make_python_tags="ctags -R --fields=+l --languages=python --python-kinds=-iv -f ./tags $(python -c "import os, sys; print(' '.join('{}'.format(d) for d in sys.path if os.path.isdir(d)))")"
 
-eval "$(pyenv virtualenv-init -)"
-if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init -)"
+# This conflicts with pyenv
+# I removed the part that manpultes the path,
+# Instead I manually edited the PATH in the direnv file
+# Hopefully, nobody else will have to do this shit
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/Users/tom/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
 fi
+unset __conda_setup
+# <<< conda initialize <<<
