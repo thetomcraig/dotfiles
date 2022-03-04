87ead7a1ebfe3b310b0fa52c4519182fb4430b66
Tom Craig <thetomcraig@icloud.com>
2022-03-04 14:32:37 -0800

    bunch of alises

--- zsh_general_settings.sh
+++ zsh_general_settings.sh
@@ -8,18 +8,6 @@ export LC_ALL=en_US.UTF-8
 export EDITOR="/usr/local/bin/vim"
 export FZF_DEFAULT_COMMAND="rg --files"
 
-terminal_emulator=$(osascript -e 'tell application "System Events"' \
-                              -e 'set frontApp to name of first application process whose frontmost is true' \
-                              -e 'end tell')
-case $terminal_emulator in
-  kitty)
-    export TERM=xterm-kitty 
-  ;;
-  iTerm2)
-    export TERM=xterm-256color
-  ;;
-esac
-
 HISTSIZE=5000               #How many lines of history to keep in memory
 HISTFILE=~/.zsh_history     #Where to save history to disk
 SAVEHIST=5000               #Number of history entries to save to disk
@@ -44,12 +32,10 @@ alias dot="cd ${DOTFILES_LOCATION}"
 alias v="vim ${DOTFILES_LOCATION}/vim/vimrc.sh"
 alias vu="vim ${DOTFILES_LOCATION}/vim/vim-plug_settings.sh"
 alias db="cd ${HOME}/Dropbox/TomCraig"
+alias dbi="cd ${HOME}/Dropbox/TomCraig/00\ Inbox && mytree"
+alias refs="cd ${HOME}/Dropbox/TomCraig/03\ References && mytree"
+alias docs="cd ${HOME}/Dropbox/TomCraig/06\ Documents && mytree"
 alias proj="cd ${PROJECTS_ROOT}"
-alias notes=openNotes
-openNotes() {
-  cd ${HOME}/Dropbox/TomCraig/Notes\ and\ Documents
-  vim README.md
-}
 
 #t for tree
 alias t=mytree
@@ -58,61 +44,28 @@ mytree() {
   #if no first parameter,
   #and otherwise uses the
   #supplied paramter for level depth
-  exa -T -L=${1-1} 
+
+
+  local level=1
+  local directory="."
+
+  if [ "$#" -eq 1 ]; then
+    if [[ $1 =~ ^-?[0-9]+$ ]]; then
+      level=${1-1}
+    else
+      directory=${1}
+    fi
+  fi
+
+
+  exa -T -L=${level} ${directory}
 }
 #tt for maximum depth tree
 alias tt="mytree 999"
 alias s="source $DOTFILES_LOCATION/zsh.sh"
-alias b="vim ${HOME}/.zshrc"
+alias eb="vim ${HOME}/.zshrc"
+alias ebe="vim ${DOTFILES_LOCATION}/zsh_environment_settings.sh"
+alias ebg="vim ${DOTFILES_LOCATION}/zsh_general_settings.sh"
+alias ebgit="vim ${DOTFILES_LOCATION}/zsh_git_settings.sh"
 
 alias start="./start.command"
-
-
-################## 
-# HELPER FUNCTIONS
-################## 
-echoGreen() {
-  tput setaf 2
-  echo "$1"
-  tput sgr0
-}
-
-echoYellow() {
-  tput setaf 3
-  echo "$1"
-  tput sgr0
-}
-
-echoRed() {
-  tput setaf 1
-  echo "$1"
-  tput sgr0
-}
-echoCyan() {
-  tput setaf 6
-  echo "$1"
-  tput sgr0
-}
-
-promptToContinue() {
-  while true; do
-    tput bold
-    read -p "$1 (y/N) " yn
-    tput sgr0
-    if [ "$yn" = "" ]; then
-      yn='N'
-    fi
-    case $yn in
-        [Yy] ) break;;
-        [Nn] ) exit 1;;
-        * ) echo "Please answer y or n for yes or no.";;
-    esac
-  done
-}
-
-ripgrep_search() {
-  rg --files-with-matches --no-messages "${1}" . | fzf --preview "highlight -O ansi -l {} 2> /dev/null | rg --colors 'match:bg:yellow' --ignore-case --pretty --context 10 '${1}' || rg --ignore-case --pretty --context 10 '${1}' {}"
-}
-
-
-alias lsd="du -hs */ | sort -hr | head"
