8cce685e8d346ed7aeab725523b8daccf65f1a8e
Tom Craig <thetomcraig@icloud.com>
2022-04-29 21:12:05 -0700

    chit pyenv

--- zsh_environment_settings.sh
+++ zsh_environment_settings.sh
@@ -5,6 +5,17 @@
 PROJECTS_ROOT=~/.projects_root
 
 
+in_linux=false
+in_macos=false
+case "$(uname -s)" in
+   Darwin)
+     in_macos=true
+     ;;
+   Linux)
+     in_linux=true
+     ;;
+esac
+
 
 #########
 # HOST/OS
@@ -25,25 +36,6 @@ alias sshjuno="ssh pi@juno"
 
 
 
-########
-# CHIT #
-########
-eval "$(chit shell-init)"
-# cs() {
-  # chit set-theme "${1}"
-  # eval "$(chit export-env-vars)"
-  # if [ -n "$TMUX" ]; then
-    # Reload tmux environment variables
-  # tmux source-file ~/.tmux.conf
-  # fi
-# }
-cs() {
-  chit set-theme "${1}"
-  eval "$(chit export-env-vars)"
-}
-
-
-
 ###################
 # EXPORT ENV VARS #
 ###################
@@ -61,15 +53,25 @@ export TMUX_SESSION_NAME="${TMUX_SESSION_NAME}"
 ##################
 # BREW
 export PATH=/opt/homebrew/bin:$PATH
+export PATH=/opt/homebrew/sbin:$PATH
 
-# PYENV
-eval "$(pyenv init --path)"
+if $in_macos; then
+  # PYENV
+  eval "$(pyenv init --path)"
+  # PYENV VIRTUALENV
+  eval "$(pyenv virtualenv-init -)"
+  # RBENV
+  eval "$(rbenv init -)"
+fi
 
-# PYENV VIRTUALENV
-eval "$(pyenv virtualenv-init -)"
+if $in_linux; then
+  # PYENV
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+  # RBENV
+  export PATH=$HOME/.rbenv/bin:$PATH
+fi
 
-# RBENV
-eval "$(rbenv init -)"
 export PATH="/usr/local/sbin:$PATH"
 
 # NVM
@@ -83,7 +85,22 @@ export PATH="/usr/local/opt/avr-gcc@8/bin:$PATH"
 eval "$(direnv hook zsh)"
 
 
-# NEEDED???
-#export NODE_PATH=$(npm root -g)
-#export PATH="$NODE_PATH:$PATH"
-#export PATH="${HOME}/Library/Python/3.7/$PATH"
+
+########
+# CHIT #
+########
+eval "$(chit shell-init)"
+
+# cs() {
+  # chit set-theme "${1}"
+  # eval "$(chit export-env-vars)"
+  # if [ -n "$TMUX" ]; then
+    # Reload tmux environment variables
+  # tmux source-file ~/.tmux.conf
+  # fi
+# }
+
+cs() {
+  chit set-theme "${1}"
+  eval "$(chit export-env-vars)"
+}
