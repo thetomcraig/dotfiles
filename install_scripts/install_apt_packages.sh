a07aa572eb0a87fe2492e5568295da7efb8cd466
Tom Craig <thetomcraig@icloud.com>
2022-04-29 21:14:33 -0700

    apt

---
+++ install_scripts/install_apt_packages.sh
@@ -0,0 +1,9 @@
+apt install zsh
+apt install exa
+
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+export PATH=$HOME/.rbenv/bin:$PATH
+
+curl -sfL https://direnv.net/install.sh | bash
