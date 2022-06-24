89c6db6933ab389d20c4f2f3b1d50328add29229
Tom Craig <thetomcraig@icloud.com>
2022-06-23 21:50:17 -0700

    change os to yellowwq

--- p10k.sh
+++ p10k.sh
@@ -179,7 +179,7 @@
   #################################[ os_icon: os identifier ]##################################
   # OS identifier color.
   typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=232
-  typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=4
+  typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=3
   # Custom icon.
   # typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='⭐'
 
