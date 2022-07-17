6480c1fc90752e45e47d3899e11376c8d84c35c3
Tom Craig <thetomcraig@icloud.com>
2022-07-16 19:21:35 -0700

    linus friendly

---
+++ install_scripts/README_linux.sh
@@ -0,0 +1,28 @@
+# Environment Installation Guide
+
+## Packages Install
+- [X] Install Brew Packages
+  ```
+  sudo ./install_apt_packages.sh
+  ```
+  
+- [X] Install Dot Files
+  ```
+  sudo ./install_dotfiles.sh
+  ```
+
+- [X] Symlink Dot Files
+  ```
+  ./install_symlinks.sh
+  ```
+
+- [X] Install Command Line Applications
+  ```
+  sudo ./install_cli_apps.sh
+  ```
+
+
+## Developer Tools
+- Pyenv python versions
+- NVM node versions
+- Conda python versions
