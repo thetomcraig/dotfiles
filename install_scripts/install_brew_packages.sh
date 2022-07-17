246f3b3b56646ff770aef616e7fd67faa86b1741
Tom Craig <thetomcraig@icloud.com>
2022-07-16 18:06:57 -0700

    Tap for display placer

--- install_scripts/install_brew_packages.sh
+++ install_scripts/install_brew_packages.sh
@@ -3,6 +3,8 @@
 
 export PATH=/opt/homebrew/bin:$PATH
 
+brew tap jakehilborn/jakehilborn
+
 # Brew packages
 # Use the path to brew because it's not in the PATH yet
 brew install $(cat brew_packages.txt)
