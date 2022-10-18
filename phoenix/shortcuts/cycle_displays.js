96a75e64ac24a324d1a2320d7a8f6fec0f9bb294
Tom Craig <thetomcraig@icloud.com>
2022-10-17 20:07:30 -0700

    screen asignment

--- phoenix/shortcuts/cycle_displays.js
+++ phoenix/shortcuts/cycle_displays.js
@@ -1,13 +1,14 @@
-// screen ids:
-// AA4129F8-E94C-455C-A2CE-F3C7DF605E8E
-// 95458444-A649-45C1-94AA-7D8880A7144F
-// 37D8832A-2D66-02CA-B9F7-8F30A301B230
-// B2085621-B801-4448-8FB9-30E7D08C1419
-const window_ids = [
-                    "AA4129F8-E94C-455C-A2CE-F3C7DF605E8E",
-                    "95458444-A649-45C1-94AA-7D8880A7144F",
+// Internal
+// Dell 27
+// Dell 25
+// Kogoda
+// HDMI Strip
+const windowIds = [
                     "37D8832A-2D66-02CA-B9F7-8F30A301B230",
-                    "B2085621-B801-4448-8FB9-30E7D08C1419"
+                    "95458444-A649-45C1-94AA-7D8880A7144F",
+                    "AA4129F8-E94C-455C-A2CE-F3C7DF605E8E",
+                    "B2085621-B801-4448-8FB9-30E7D08C1419",
+                    "3913815D-56F0-4FE7-90F7-6CB91892A81D"
                    ];
 
 function frameRatio(a, b){
@@ -24,29 +25,15 @@ function frameRatio(a, b){
   };
 };
 
-setKeyHandler ( 'x', HYPER, () => {
-
+function moveToDisplayWithUUID(uuid) {
   const window = Window.focused ();
-
   if ( !window ) return;
 
   const oldScreen = window.screen(); 
-  const newScreen = oldScreen.next();
 
-  // console.log(window);
-  // console.log(window.screen());
-  // console.log(oldScreen.spaces());
-  // console.log(oldScreen.currentSpace());
-  // console.log(oldScreen.windows());
-  // console.log(oldScreen.identifier());
-  // console.log(oldScreen.screenForIdentifier("37D8832A-2D66-02CA-B9F7-8F30A301B230"));
-  // console.log("spaces");
-  // console.log(oldScreen.spaces());
-  // console.log(oldScreen.spaces().all());
+  const newScreen = Screen.all().find(screen => screen.identifier() === uuid)
 
-
-
-  if(oldScreen.isEqual(newScreen)){
+  if(oldScreen.isEqual(newScreen) || newScreen === undefined) {
      return; 
   }
 
@@ -56,29 +43,20 @@ setKeyHandler ( 'x', HYPER, () => {
   )
 
   window.setFrame(ratio(window.frame()));
+}
 
+setKeyHandler ('1', HYPER, () => {
+  moveToDisplayWithUUID(windowIds[0])
 });
-
-setKeyHandler ( '1', HYPER, () => {
-
-  const window = Window.focused ();
-
-  if ( !window ) return;
-
-  const oldScreen = window.screen(); 
-  const newScreen = oldScreen.next();
-
-  if(oldScreen.isEqual(newScreen)){
-     return; 
-  }
-
-  const ratio = frameRatio(
-     oldScreen.flippedVisibleFrame(), 
-     newScreen.flippedVisibleFrame(),
-  )
-
-  window.setFrame(ratio(window.frame()));
-
+setKeyHandler ('2', HYPER, () => {
+  moveToDisplayWithUUID(windowIds[1])
 });
-
-
+setKeyHandler ('3', HYPER, () => {
+  moveToDisplayWithUUID(windowIds[2])
+});
+setKeyHandler ('4', HYPER, () => {
+  moveToDisplayWithUUID(windowIds[3])
+});
+setKeyHandler ('5', HYPER, () => {
+  moveToDisplayWithUUID(windowIds[3])
+});
