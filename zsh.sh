93b85a1b5900320249260e8cb0e545f16b58ae90
Tom Craig <thetomcraig@icloud.com>
2023-10-13 17:29:22 -0700

    small updates

--- zsh.sh
+++ zsh.sh
@@ -1,3 +1,10 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
 # vim: set syntax=zsh
 # au BufReadPost zsh.sh set ft=zsh.rc
 #
@@ -6,13 +13,11 @@ export DOTFILES_LOCATION="${HOME}/dotfiles"
 
 printf "Loading env settings... "
 source $DOTFILES_LOCATION/zsh_environment_settings.sh
-printf "Done"
 source $DOTFILES_LOCATION/zsh_general_settings.sh
 source $DOTFILES_LOCATION/zsh_omz_settings.sh
 source $DOTFILES_LOCATION/zsh_git_settings.sh
 source $DOTFILES_LOCATION/zsh_python_settings.sh
 source $DOTFILES_LOCATION/tmux/zsh_tmux_settings.sh
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
