6664b8a4274fb2e356678039e22be255030e5beb
Tom Craig <thetomcraig@icloud.com>
2024-07-06 10:56:34 -0700

    tree stuff

--- zsh_environment_settings.sh
+++ zsh_environment_settings.sh
@@ -72,6 +72,10 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+#########
+# CARGO #
+#########
+export PATH="${HOME}/.cargo/bin:$PATH"
 
 ########
 # CHIT #
