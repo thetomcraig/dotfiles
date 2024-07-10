aa74ef3049c5cf5e1ce93451526f3326880ccabb
Tom Craig <thetomcraig@icloud.com>
2024-07-09 19:21:10 -0700

    add ssh aliases and racher settings

--- zsh_aya_settings.sh
+++ zsh_aya_settings.sh
@@ -1,7 +1,9 @@
-  export PATH="/opt/homebrew/opt/postgresql@15/bin:$PATH"
+alias sshdev="ssh polarisadmin@10.184.9.4"
+alias sshsand="ssh -i ~/.ssh/polaris_sand_vm_ssh_key.txt polarisadmin@10.183.9.4"
+alias sshint="ssh -i ~/.ssh/polaris_int_vm_ssh_key.txt polarisadmin@10.185.9.4"
 
-  ### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
-  export PATH="/Users/tomcraig/.rd/bin:$PATH"
-  ### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
+export PATH="/opt/homebrew/opt/postgresql@15/bin:$PATH"
 
-
+### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
+export PATH="/Users/tomcraig/.rd/bin:$PATH"
+### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
