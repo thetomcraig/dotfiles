8c307807118bfc123d4c88ee53fb2ba46721d6a8
Tom Craig <thetomcraig@icloud.com>
2024-08-25 17:38:51 -0700

    work stuff

--- zsh_aya_settings.sh
+++ zsh_aya_settings.sh
@@ -1,9 +1,5 @@
-alias sshdev="ssh polarisadmin@10.184.9.4"
-alias sshsand="ssh -i ~/.ssh/polaris_sand_vm_ssh_key.txt polarisadmin@10.183.9.4"
-alias sshint="ssh -i ~/.ssh/polaris_int_vm_ssh_key.txt polarisadmin@10.185.9.4"
+alias sshdev="tmux rename-window 'DEV'; ssh polarisadmin@10.184.9.4"
+alias sshsand="tmux rename-window 'SAND'; ssh -i ~/.ssh/polaris_sand_vm_ssh_key.txt polarisadmin@10.183.9.4"
+alias sshint="tmux rename-window 'INT'; ssh -i ~/.ssh/polaris_int_vm_ssh_key.txt polarisadmin@10.185.9.4"
 
 export PATH="/opt/homebrew/opt/postgresql@15/bin:$PATH"
-
-### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
-export PATH="/Users/tomcraig/.rd/bin:$PATH"
-### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
