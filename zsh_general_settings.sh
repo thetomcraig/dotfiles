e238997f6de25e71d266846c79366bda620f60d9
Tom Craig <thetomcraig@icloud.com>
2024-08-30 17:02:49 -0700

    remove k8s containers

--- zsh_general_settings.sh
+++ zsh_general_settings.sh
@@ -28,7 +28,7 @@ alias cpr="rsync -Pa $1 $2"
 
 alias dcu="docker compose up"
 alias dcd="docker compose down"
-alias dps="docker ps --format 'table{{.Names}}\t{{.Image}}'"
+alias dps="docker ps --format 'table{{.Names}}\t{{.Image}}' | grep -v k8s_"
 alias doco="docker"
 
 dockerbash() {
