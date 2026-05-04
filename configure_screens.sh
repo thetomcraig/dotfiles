f2de9f8db63d458da1930b1a4091dc687c00578e
Tom Craig <thetomcraig@icloud.com>
2026-05-04 09:14:43 -0700

    configure screens

--- configure_screens.sh
+++ configure_screens.sh
@@ -35,6 +35,7 @@ screenarray=( $(system_profiler SPDisplaysDataType) )
 count=0
 displayNum=0
 until [ $count -gt $((${#screenarray[@]} + 1 )) ]; do
+  echo "${screenarray[count]}"
   if [[ "${screenarray[count]}" == *"${dell27}"* ]]; then
     $desktoppr $displayNum "${wallpaperFolder}/wave_1_large.png"
     displayNum=$(( $displayNum + 1 ))
@@ -59,11 +60,6 @@ done
 
 
 
-# display placer command for screens with top strip:
-# displayplacer "id:35120216-2552-4D1B-A398-C1EC8C9A4D7D res:2304x1296 hz:60 color_depth:8 enabled:true scaling:on origin:(0,0) degree:0" "id:AF1BCCA1-F0C7-48F2-AD48-78343323861D res:1152x2048 hz:60 color_depth:8 enabled:true scaling:on origin:(2304,-440) degree:270" "id:12B5BF48-FC89-42C3-AF36-12434AA8CA8B res:3008x862 hz:60 color_depth:8 enabled:true scaling:on origin:(-704,-862) degree:180" "id:3344420E-5B08-40B6-9BE1-248F1B14B280 res:1920x1080 hz:60 color_depth:8 enabled:true scaling:on origin:(-1920,0) degree:0"
-
-
-
 ###############
 # ARRANGEMENT #
 ###############
@@ -74,15 +70,12 @@ done
 # Personal computer, no HDMI strip and YESportable
 # displayplacer "id:95458444-A649-45C1-94AA-7D8880A7144F res:2304x1296 hz:60 color_depth:8 scaling:on origin:(0,0) degree:0" "id:912EDD5D-2AB5-4DAA-9A74-674C7BBD99FD res:1152x2048 hz:60 color_depth:8 scaling:on origin:(2304,-340) degree:270" "id:B2085621-B801-4448-8FB9-30E7D08C1419 res:1344x756 hz:60 color_depth:8 scaling:off origin:(-1664,1080) degree:0" "id:397DC1C5-E579-44E5-B735-D35C282D16FF res:1920x1080 hz:60 color_depth:8 scaling:off origin:(-1920,0) degree:0"
 #
-#
-#
 
 numScreens=$(system_profiler SPDisplaysDataType | grep -c "Display Type")
 if [ $numScreens -eq 1 ];
 then
   displayplacer "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1728x1117 hz:120 color_depth:8 enabled:true scaling:on origin:(0,0) degree:0"
-else
-  displayplacer "id:95458444-A649-45C1-94AA-7D8880A7144F res:2560x1440 hz:60 color_depth:8 enabled:true scaling:on origin:(0,0) degree:0" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1312x848 hz:120 color_depth:8 enabled:true scaling:on origin:(-1312,0) degree:0" "id:8C00957C-866B-4464-AECE-0DE2FEAB9F0F res:1440x2560 hz:60 color_depth:8 enabled:true scaling:off origin:(2560,-933) degree:270"
+else 
+  displayplacer "id:35120216-2552-4D1B-A398-C1EC8C9A4D7D res:2560x1440 hz:60 color_depth:8 enabled:true scaling:on origin:(0,0) degree:0" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1728x1117 hz:120 color_depth:8 enabled:true scaling:on origin:(2560,0) degree:0" "id:DB3CA055-0148-492C-A78B-B37C5307CC18 res:1440x2560 hz:60 color_depth:8 enabled:true scaling:off origin:(-1440,-741) degree:90"
+  displayplacer "id:35120216-2552-4D1B-A398-C1EC8C9A4D7D res:2560x1440 hz:60 color_depth:8 enabled:true scaling:on origin:(0,0) degree:0" "id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1728x1117 hz:120 color_depth:8 enabled:true scaling:on origin:(2560,0) degree:0" "id:DB3CA055-0148-492C-A78B-B37C5307CC18 res:1440x2560 hz:60 color_depth:8 enabled:true scaling:off origin:(-1440,-741) degree:90"
 fi
-# It takes two tries to get this right because of bugginess in the resolution
-# DOES it??
