6c5047fc79531281d8d8db1c6f43f843982c7463
Tom Craig <thetomcraig@icloud.com>
2026-08-05 12:25:42 -0700

    codex symlinks

--- install_scripts/install_symlinks.sh
+++ install_scripts/install_symlinks.sh
@@ -10,38 +10,42 @@ if [[ "${ARCH}" == *"${mac}"* ]]; then
   PROJECTS_ROOT="${HOME}/Documents/Projects"
   mkdir -p ${PROJECTS_ROOT}
 
-  rm -f -- ~/.PROJECTS_ROOT
+  \rm -f -- ~/.PROJECTS_ROOT
   ln -s $PROJECTS_ROOT .projects_root
 fi
 
 
-rm -f -- ~/.zshrc
+\rm -f -- ~/.zshrc
 ln -s $DOTFILES_LOCATION/zsh.sh .zshrc
 
-rm -f -- ~/.p10k.zsh
+\rm -f -- ~/.p10k.zsh
 ln -s $DOTFILES_LOCATION/p10k.sh .p10k.zsh
 
-rm -f -- ~/.vimrc
+\rm -f -- ~/.vimrc
 ln -s $DOTFILES_LOCATION/vim/vimrc.sh .vimrc
 
-rm -f -- ~/.remarkrc.json
+\rm -f -- ~/.remarkrc.json
 ln -s $DOTFILES_LOCATION/remarkrc.json .remarkrc.json
 
-rm -f -- ~/.prettierrc.json
+\rm -f -- ~/.prettierrc.json
 ln -s $DOTFILES_LOCATION/remarkrc.json .prettierrc.json
 
-rm -f -- ~/.tigrc
+\rm -f -- ~/.tigrc
 ln -s $DOTFILES_LOCATION/tigrc.sh .tigrc
 
-rm -f -- ~/.gvimrc
+\rm -f -- ~/.gvimrc
 ln -s $DOTFILES_LOCATION/vim/gvimrc.sh .gvimrc
 
-rm -f -- ~/.gitconfig
+\rm -f -- ~/.gitconfig
 ln -f $DOTFILES_LOCATION/git_config_global.sh .gitconfig
 
-rm -f -- ~/.vim/spell
+\rm -f -- ~/.vim/spell
 ln -s $DOTFILES_LOCATION/vim/spell .vim/spell
 
-rm -f -- ~/.tmux.conf
+\rm -f -- ~/.tmux.conf
 ln -s $DOTFILES_LOCATION/tmux/tmux.conf .tmux.conf
 
+\rm -f -- ~/.codex/AGENTS.md
+ln -s $DOTFILES_LOCATION/codex/AGENTS.md .codex/AGENTS.md
+\rm -f -- ~/.codex/skills
+ln -s $DOTFILES_LOCATION/codex/skills .codex/skills
